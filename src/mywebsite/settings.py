00ee48de58b3635a3376549cbe2643eff7fcec65
DiamondLuver <112000019+DiamondLuver@users.noreply.github.com>
2023-04-26 12:45:56 +0700

    update homepage
    add fontawesome and bootstrap library

--- src/mywebsite/settings.py
+++ src/mywebsite/settings.py
@@ -31,13 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'homepage'  #pich
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'homepage',  #pich
+    'bootstrap5',
+    'fontawesomefree',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
