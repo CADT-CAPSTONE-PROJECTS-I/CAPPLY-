9a5bf0a0068491c440e06f2175ec217ba5677985
DiamondLuver <112000019+DiamondLuver@users.noreply.github.com>
2023-05-05 13:13:16 +0700

    update model

--- src/category/models.py
+++ src/category/models.py
@@ -1,9 +1,6 @@
 import django
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
-from django.utils import timezone
-from datetime import datetime
 # Create your models here.
 CONTINENT = [
     ("ASIA",'Asia'), 
@@ -26,24 +23,24 @@ CONTINENT = [
 #         return self.name 
     
 class Country(models.Model):
-    # id = models.BigAutoField(primary_key=True)
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    description = models.TextField()
     continent = models.CharField(max_length=5, choices=CONTINENT)
     def __str__(self):
         return self.name
     
 
 class Scholarship(models.Model):
-    # id = models.BigAutoField(primary_key=True)
-    title = models.CharField("scholarship's title",max_length=255)
-    tag = models.CharField(max_length=255)
-    date_open = models.DateField()
-    date_close = models.DateField()
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
-
+    id = models.BigAutoField(primary_key=True)
+    level = models.CharField(max_length=255)
+    study_field = models.CharField(max_length=255)
+    school = models.CharField(max_length=255)
+    deadline = models.DateField()
+    more_info = models.TextField()
+    link_web = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
     def __str__(self):
-        return self.title 
+        return self.school 
     
     # file = models.FileField(upload_to="documents/%Y/%m/%d", validators=[validate_file_extension])
 
