ba9579c8c4d3b163ec5426ab73cca2f24e313e74
DiamondLuver <112000019+DiamondLuver@users.noreply.github.com>
2023-05-18 21:53:57 +0700

    change admin site name

--- src/mywebsite/urls.py
+++ src/mywebsite/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include,reverse_lazy
+from django.urls import path, include
 from homepage.views import homepage
-from django.views.generic.base import RedirectView
 from django.contrib.auth import views as auth_views #new
 
 urlpatterns = [
