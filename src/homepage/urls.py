30f92f6867ad747623c283f444d956e0132c59f6
DiamondLuver <112000019+DiamondLuver@users.noreply.github.com>
2023-05-22 06:53:33 +0700

    url of hompage

--- src/homepage/urls.py
+++ src/homepage/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path
-from homepage.views import homepage, register, login, logout, profile, show_category
-=======
-from django.urls import path, include
 from homepage.views import homepage, register, login, logout, profile, profile_edit, show_category
 
->>>>>>> bc357e2df207a594a8d24c0ebef968fed654675a
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',homepage, name="home"),
