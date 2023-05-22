ca9ea00a7e668642ad97f8cc617eb1c03d2286e8
DiamondLuver <112000019+DiamondLuver@users.noreply.github.com>
2023-05-22 14:48:16 +0700

    add view

--- src/category/views.py
+++ src/category/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Scholarship
+from django.core.paginator import Paginator
 # Create your views here.
+
+def list_scholarship(request):
+    # scholarship_lists = Scholarship.objects.all().order_by('?')
+    scholarship_lists = Scholarship.objects.all()
+    # set up pagination
+    p = Paginator(Scholarship.objects.all(), 20)
+    page = request.GET.get('page')
+    scholarships = p.get_page(page)
+    return render(request,'category/category.html',{'scholarship_lists': scholarship_lists,
+    'scholarships': scholarships})
+
