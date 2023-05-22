be418ad5134385c769cbe0a55ebb39c0114af152
DiamondLuver <112000019+DiamondLuver@users.noreply.github.com>
2023-05-22 11:00:23 +0700

    Add scholarship detail and view

--- src/mywebsite/urls.py
+++ src/mywebsite/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from homepage.views import homepage
+# from category.views import 
 from django.contrib.auth import views as auth_views #new
 
 urlpatterns = [
     path('admin/login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'), #new
     path('admin/', admin.site.urls),
-    path('',homepage, name="home"),
+    path('', homepage, name="home"),
     path('', include('homepage.urls')),
+    path('', include('category.urls')),
 ]
