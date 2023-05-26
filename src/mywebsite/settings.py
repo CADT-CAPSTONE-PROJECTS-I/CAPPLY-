5f47fc6528fb045581b49b9d60aa84c104e33aa3
DiamondLuver <112000019+DiamondLuver@users.noreply.github.com>
2023-05-26 23:02:31 +0700

    Update settings

--- src/mywebsite/settings.py
+++ src/mywebsite/settings.py
@@ -130,11 +130,3 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-from decouple import config
-# EMAIL CONFIGURATION (for contact form)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = 'your_password>'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
