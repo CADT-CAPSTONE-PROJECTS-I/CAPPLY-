9393f2f6a52bf574bd0fd3a690a3c300e5ef3038
deborahsom <diadeb18@gmail.com>
2023-05-27 03:23:38 +0700

    auto create profile

--- src/category/models.py
+++ src/category/models.py
@@ -1,6 +1,7 @@
 import django
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 # Create your models here.
 CONTINENT = [
     ("ASIA",'Asia'), 
@@ -56,9 +57,13 @@ class Profile(models.Model):
     user = models.OneToOneField(User, null  =True, on_delete = models.CASCADE)
     profile_pic = models.ImageField(default='static\images\profile_pics\Default.png', upload_to='static\images\profile_pics')
     bio = models.TextField(default="This user is lazy and has nothing to say.", max_length=124, null = True, blank = True)
-    school = models.TextField(max_length=30, null = True, blank = True)
-    phone = models.TextField(max_length=10, null = True, blank = True)
-    major = models.CharField(max_length=40, choices=MAJOR_LIST, default='N/A', null = True, blank = True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        
+post_save.connect(create_profile, sender=User)
