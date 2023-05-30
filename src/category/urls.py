2338a176111db3a89f8f8dd80e29ba53f21feb76
Deborah <diadeb18@gmail.com>
2023-05-30 12:18:18 +0700

    Update urls.py

--- src/category/urls.py
+++ src/category/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
      path('scholarship', list_scholarship, name = 'scholarship'),
      path('scholarship/<slug:slug>/', ScholarshipDetailView.as_view(), name='scholarship_detail'),
      path('scholarship/tag/<str:country>', search_tag, name = 'scholarship_tag'),
+     path('profile_edit/', UserEditView.as_view(), name="profile_edit"),
+
 ]
